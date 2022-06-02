0a3ddaa5ee425f63bf51ebcab2ce0e7954a0bb9f
agus5312 <85003261+agus5312@users.noreply.github.com>
2022-06-02 18:31:03 -0300

    Nuevo Bosque y luna roja

--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Moon : MonoBehaviour
 {
-    [SerializeField] int velocity;
-
+    Light LMoon;
+    Renderer rend;
+    float a = 1;
+    //[SerializeField] int velocity;
+    void Start()
+    {
+        LMoon = GetComponent<Light>();
+        rend = GetComponentInChildren<Renderer>();
+    }
 
     void Update()
     {
-        transform.Rotate(velocity * Time.deltaTime, 0, 0);
+        //transform.Rotate(velocity * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(Cambiar());
+        }
+    }
 
+    IEnumerator Cambiar()
+    {
+        yield return new WaitForSeconds(0.1f); 
+
+        for (int i = 0; i < 100; i++)
+        {
+            a -= 0.01f;
+            rend.material.SetColor("_EmissionColor", new Color(1, a, a));
+            LMoon.color = new Color(1, a, a);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     
