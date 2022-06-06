c0374e2aeeabd23014f697226700b7848389a434
agus5312 <85003261+agus5312@users.noreply.github.com>
2022-06-06 17:05:19 -0300

    Retoques al menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,29 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     GameObject luzLinterna;
-
+    [SerializeField] GameObject main;
+    [SerializeField] GameObject panel;
     private void Start()
     {
         luzLinterna = GameObject.FindGameObjectWithTag("Luz");
         luzLinterna.SetActive(false);
-        StartCoroutine(PrenderLinterna(Random.Range(0.1f, 2)));
+        StartCoroutine(PrenderLinterna(Random.Range(0.1f, 1)));
+        main.SetActive(true);
+        panel.SetActive(false);
     }
 
     IEnumerator PrenderLinterna(float tiempo)
     {
         yield return new WaitForSeconds(tiempo);
         luzLinterna.SetActive(true);
-        StartCoroutine(ApagarLinterna(Random.Range(0.1f,2)));
+        StartCoroutine(ApagarLinterna(Random.Range(0.1f,1)));
     }
 
     IEnumerator ApagarLinterna(float tiempo)
     {
         yield return new WaitForSeconds(tiempo);
         luzLinterna.SetActive(false);
-        StartCoroutine(PrenderLinterna(Random.Range(0.1f, 2)));
+        StartCoroutine(PrenderLinterna(Random.Range(0.5f, 1.5f)));
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+
+    public void Options()
+    {
+        main.SetActive(false);
+        panel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void QuitMenu()
+    {
+        main.SetActive(true);
+        panel.SetActive(false);
     }
 }
