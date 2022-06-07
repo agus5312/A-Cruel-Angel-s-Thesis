03c9b89e5acf2435f1c922fce61b0de650286fbb
agus5312 <85003261+agus5312@users.noreply.github.com>
2022-06-07 00:24:15 -0300

    Puerta y cercania a objetos

--- Assets/Scripts/Objetos/ObjetoEnSuelo.cs
+++ Assets/Scripts/Objetos/ObjetoEnSuelo.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class ObjetoEnSuelo : MonoBehaviour
 {
     public GameObject indicador;
-
+    GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void OnMouseEnter()
     {
+        if(Vector3.Distance(transform.position,player.transform.position) < 3)
+        {
             indicador.SetActive(true);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 3)
+        {
+            indicador.SetActive(true);
+        }
+        else
+        {
+            indicador.SetActive(false);
+        }
     }
 
     private void OnMouseExit()
